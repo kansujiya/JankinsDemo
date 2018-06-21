968bcadea017cbe2411280ecb724cf2f3730990d
suresh-kansujiya <suresh.kansujiya@gmail.com>
2018-06-21 12:49:21 +0530

    templet project added
    templet project added

---
+++ JankinsDemoTests/JankinsDemoTests.swift
@@ -0,0 +1,28 @@
+//
+//  JankinsDemoTests.swift
+//  JankinsDemoTests
+//
+//  Created by Suresh Kansujiya on 21/06/18.
+//  Copyright © 2018 Suresh Kansujiya. All rights reserved.
+//
+
+import XCTest
+@testable import JankinsDemo
+
+class JankinsDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testAddition() {
+        let flag = true
+        XCTAssertTrue(flag)
+    }
+}
